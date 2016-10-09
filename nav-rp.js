078744a548658f6f3dc1475b566b0f33f86a529d
Mito <bdinh2@ocdsb.ca>
2016-10-08 22:05:30 -0400

    new nav button

--- nav-rp.js
+++ nav-rp.js
@@ -1,8 +1,14 @@
 function mobile() {
     var x = document.getElementById("id-nav-links");
+    var y = document.getElementById("mobile");
     if (x.className === "nav-links") {
         x.className += " responsive";
     } else {
         x.className = "nav-links";
+    }
+    if (y.className === "middle") {
+        y.className += " on";
+    } else {
+        y.className = "middle";
     }
 }
