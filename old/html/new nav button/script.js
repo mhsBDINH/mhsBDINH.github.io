f2ff67f7849409e57081c088ee5a9625b4878172
mhsBDINH <bdinh2@ocdsb.ca>
2017-02-28 21:52:11 -0500

    React Change

--- old/html/new nav button/script.js
+++
@@ -1,8 +0,0 @@
-function mobile() {
-    var x = document.getElementsByClassName("middle");
-    if (x.className === "middle") {
-        x.className += "on";
-    } else {
-        x.className = "middle";
-    }
-}
--- page/javascript/nav-rp.js
+++
@@ -1,8 +0,0 @@
-function mobile() {
-    var x = document.getElementById("id-nav-links");
-    if (x.className === "nav-links") {
-        x.className += " responsive";
-    } else {
-        x.className = "nav-links";
-    }
-}
